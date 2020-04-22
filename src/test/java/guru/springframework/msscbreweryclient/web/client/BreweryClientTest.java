1d7a1514c7efa3f1119333f67c253e537b1bcc7a
Lucas Ramos Zilocchi <lucaszilocchi1@thomsonreuters.com>
2020-04-22 07:57:26 -0300

    Adding Client With restTemplate

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    private BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = client.getBeerById(UUID.randomUUID());
+        assertNotNull(beerDto);
+    }
+
+    @Test
+    void testSavedNewBeer(){
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        URI uri = client.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateBeer(){
+        BeerDto beerDto = BeerDto.builder().beerName("Updated Beer").build();
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void testDeleteBeer(){
+        client.deleteBeer(UUID.randomUUID());
+
+    }
+
+}
